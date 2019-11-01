194e93ed335497f984dbf308fac4b83884fb86d2
Github9019 <49651650+Github9019@users.noreply.github.com>
2019-11-01 21:37:28 +0200

    Add files via upload

---
+++ index — копия.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MafiaCoup - Новая мафия!</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/MyFrimVersion0.2/styleFrim.css">
+	<link rel="stylesheet" type="text/css" href="/style7.css">
+</head>
+<body>
+	<main class="w-100">
+		<h1 class="text-align-center"><a href="/index.php">MafiaCoup - Новая мафия!</a></h1>
+		<div class="FORM">
+			<form action="/User.php" method="POST">
+				<h2>Автаризацыя</h2>
+				<input type="text" name="NameLogin" placeholder="Ваш логин" class="inputFORM">
+				<input type="password" name="PasswordLogin" placeholder="Ваш пороль" class="inputFORM">
+				<input type="text" name="checkIteration" value="Login" class="display_none">
+				<div class="w-100 text-align-center">
+					<button>Автаризацыя</button>
+					<br>
+					<span class="w-100">Нет аккаунта?<a href="/Register.php"> Зарегестрируйся!</a></span>
+				</div>			
+			</form>
+			
+		</div>
+	</main>	
+	<script type="text/javascript" src="/MyFrimVersion0.2/JsFrim.js"></script>
+	<script type="text/javascript"></script>
+</body>
+</html>
